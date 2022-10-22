ade8a9cd9e655f9a54f2426944fca1081e7af6d6
koto-games <116263431+koto-games@users.noreply.github.com>
2022-10-22 15:54:04 +0500

    1

---
+++ sada.py
@@ -0,0 +1,71 @@
+import os
+USE = os.environ.get( "USERNAME" )
+print('''
+    ─────────────────────────────────────────────────────────────
+    ─██████████████─██████████████─████████████───██████████████─
+    ─██░░░░░░░░░░██─██░░░░░░░░░░██─██░░░░░░░░████─██░░░░░░░░░░██─
+    ─██░░██████████─██░░██████░░██─██░░████░░░░██─██░░██████░░██─
+    ─██░░██─────────██░░██──██░░██─██░░██──██░░██─██░░██──██░░██─
+    ─██░░██████████─██░░██████░░██─██░░██──██░░██─██░░██████░░██─
+    ─██░░░░░░░░░░██─██░░░░░░░░░░██─██░░██──██░░██─██░░░░░░░░░░██─
+    ─██████████░░██─██░░██████░░██─██░░██──██░░██─██░░██████░░██─
+    ─────────██░░██─██░░██──██░░██─██░░██──██░░██─██░░██──██░░██─
+    ─██████████░░██─██░░██──██░░██─██░░████░░░░██─██░░██──██░░██─
+    ─██░░░░░░░░░░██─██░░██──██░░██─██░░░░░░░░████─██░░██──██░░██─
+    ─██████████████─██████──██████─████████████───██████──██████─
+    ─────────────────────────────────────────────────────────────
+''')
+
+if os.path.exists("C:/Users/{}/Documents/woss".format(USE)):
+    pass
+else:
+    os.mkdir("C:/Users/{}/Documents/woss".format(USE))
+while True:
+    www = input('[>>')
+    if www == 'h':
+        print('''# re === создать репозиторий
+# sh === показать все созданные репозитории
+# rh === прочитать репозиторий
+# cre === очистить репозиторий и записать текст''')
+    ww = www.split(': ')
+    if ww[0] == 're':
+        if os.path.exists("C:/Users/"+USE+"/Documents/woss/{}.rw".format(ww[1])):
+            print('репозиторий уже существует')
+        else:
+            fdafsf = open("C:/Users/"+USE+"/Documents/woss/{}.rw".format(ww[1]), 'w', -1, 'utf-16be')
+            fdafsf.write("")
+            fdafsf.close()
+            print('репозиторий '+ww[1]+' создан')
+    
+    elif ww[0] == 'sh':
+        g = os.listdir("C:/Users/{}/Documents/woss".format(USE))
+        g1 = 0
+        print('')
+        while g1 < len(g):
+            oo = g[g1]
+            oo = oo.replace('.rw','')
+            print(g1+1,' >',oo)
+            g1 = g1+1
+        print('')
+
+    elif ww[0] == 'rh':
+        if os.path.exists("C:/Users/"+USE+"/Documents/woss/{}.rw".format(ww[1])):
+            fda = open("C:/Users/"+USE+"/Documents/woss/{}.rw".format(ww[1]), 'r', -1, 'utf-16be')
+            ur = fda.read()
+            if ur != '':
+                print(ur)
+            else:
+                print('репозиторий пустой')
+            fda.close()
+        else:
+            print('какого репозитория нету')
+
+    elif ww[0] == 'cre':
+        ddd = ww[1].split(',')
+        if os.path.exists("C:/Users/"+USE+"/Documents/woss/{}.rw".format(ddd[0])):
+            fdafsf = open("C:/Users/"+USE+"/Documents/woss/{}.rw".format(ddd[0]), 'w', -1, 'utf-16be')
+            fdafsf.write(ddd[1])
+            fdafsf.close()
+            print('текст сохранен')
+        else:
+            print('какого репозитория нету')
